4116a48762785860b1c94a284775d7d6f58b4a41
Axe <ngsmo@iscte-iul.pt>
2021-07-21 11:35:42 +0100

    add ArrayTransport Winston

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,12 +1,15 @@
-const winston = require('winston');
+const winston = require("winston");
+const ArrayTransport = require("winston-array-transport");
 
 class Logger {
 
     constructor(app) {
         this.app = app;
+        this.logs = new Array();
         this.logger = winston.createLogger({
             transports: [
-                new winston.transports.Console()
+                new winston.transports.Console(),
+                new ArrayTransport({ array: this.logs, json: true, level: 'error' })
             ]
         });
     }

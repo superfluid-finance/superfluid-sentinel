9b0aeaea1053059f60a61a0214153306d76633ae
Axe <ngsmo@iscte-iul.pt>
2021-09-07 19:12:57 +0300

    multi changes

--- src/web3client/liquidator.js
+++ src/web3client/liquidator.js
@@ -1,180 +1,157 @@
-const { Op } = require("sequelize");
-const EstimationModel = require("../database/models/accountEstimationModel");
-const AgreementModel = require("../database/models/agreementModel");
+const utils = require("../utils/utils");
 
-function promiseTimeout(promise, ms) {
-
-    let timeout = new Promise((resolve, reject) => {
-        let id = setTimeout(() => {
-            clearTimeout(id);
-            reject(new Error("timeout rejection"))
-        }, ms)
-    });
-
-    // Returns a race between timeout and promise
-    return Promise.race([
-        promise,
-        timeout
-    ]);
-}
-
-class TxBuilder {
+class Liquidator {
 
     constructor(app) {
         this.app = app;
-        this.timeout = 10000//this.app.config.TIMEOUT_FN;
-        this.runningMux = false;
+        this.timeout = this.app.config.TIMEOUT_FN;
+        this.runningMux = 0;
+        this.splitBatch = 10;
+        this.clo = this.app.config.CLO_ADDR;
+        this.txDelay = this.app.config.LIQUIDATION_DELAY;
+        if(this.clown !== undefined) {
+            this.app.logger.info("liquidator - adding non clown delay (15min)");
+            this.txDelay += 900;
+        }
     }
 
     async start() {
-        if(this.runningMux) {
-            this.app.logger.warn("running liquidation skip - already running");
-            return;
-        }
-        this.runningMux = true;
-        let checkDate = this.app.time.getTimeWithDelay(this.app.config.LIQUIDATION_DELAY);
-        const estimations  = await EstimationModel.findAll({
-            attributes: ['address', 'superToken', 'zestimation'],
-            where:
-            {
-                [Op.or]: [
-                    { now : true},
-                    { [Op.and]: [
-                        {
-                            zestimation: { [Op.gt]: 0 }
-                        },
-                        {
-                            zestimation: { [Op.lte]: checkDate}
-                        }
-                      ]
-                    }
-                ]
+        try {
+            this.app.logger.info("running liquidation job")
+            if(this.runningMux > 0) {
+                this.runningMux--;
+                this.app.logger.warn(`skip liquidation.start() - ${this.runningMux}`);
+                return;
             }
-        });
+            this.runningMux = 10;
+            const checkDate = this.app.time.getTimeWithDelay(this.app.config.LIQUIDATION_DELAY);
+            const haveBatchWork = await this.app.db.queries.getNumberOfBatchCalls(checkDate);
+            const work = await this.app.db.queries.getLiquidations(checkDate);
+            if(haveBatchWork.length > 0) {
+                //await this.multiTermination(haveBatchWork, work);
+                await this.singleTerminations(work);
+            } else {
+                await this.singleTerminations(work);
+            }
+        } catch(err) {
+            this.app.logger.error(`liquidator.start() - ${err}`);
+        } finally {
+            this.runningMux = 0;
+        }
+    }
+
+    async singleTerminations(work) {
+
         const wallet = this.app.client.getAccount();
         const chainId = await this.app.client.getNetworkId();
         let networkAccountNonce = await this.app.client.web3.eth.getTransactionCount(wallet.address);
-        const gasPrice = await this.app.client.estimateGasPrice();
-        for(const est of estimations) {
-            if(new Date(est.zestimation) <= checkDate) {
-                est.recalculate = true;
-                await est.save();
-                let flows  = await AgreementModel.findAll({
-                    where: {
-                        sender: est.address
+        const BaseGasPrice = await this.app.client.estimateGasPrice();
+
+        for(const job of work) {
+            await this.app.protocol.checkFlow(job.superToken, job.sender, job.receiver);
+            if(await this.app.protocol.isAccountCriticalNow(job.superToken, job.sender))
+            {
+                try {
+                    const tx = this.app.protocol.generateDeleteFlowABI(job.superToken, job.sender, job.receiver);
+                    const txObject = {
+                        retry : 1,
+                        step : 1.15,
+                        target: this.app.client.sf._address,
+                        flowSender: job.sender,
+                        flowReceiver: job.receiver,
+                        superToken: job.superToken,
+                        tx: tx,
+                        gasPrice: BaseGasPrice,
+                        nonce: networkAccountNonce,
+                        chainId: chainId
                     }
-                });
-
-                for(const flow of flows) {
-                    console.log(`token:${flow.superToken} sender: ${flow.sender} -> ${flow.receiver}`);
-                    const tx = this.app.client.sf.methods.callAgreement(
-                        this.app.client.CFAv1._address,
-                        this.app.client.CFAv1.methods.deleteFlow(
-                            flow.superToken,
-                            flow.sender,
-                            flow.receiver,
-                            "0x").encodeABI(),
-                        "0x"
-                    ).encodeABI();
-
-                    if(await this.app.client.superTokens[flow.superToken]
-                        .methods.isAccountCriticalNow(est.address).call())
-                    {
-                        try {
-                            const txObject = {
-                                retry : 1,
-                                step : 1.15,
-                                target: this.app.client.sf._address,
-                                flowSender: flow.sender,
-                                flowReceiver: flow.receiver,
-                                superToken: flow.superToken,
-                                flowRate: flow.flowRate,
-                                tx: tx,
-                                gasPrice: gasPrice,
-                                nonce: networkAccountNonce,
-                                chainId: chainId
-                            }
-                            //simulate tx
-                            const gas = await this.estimateGasLimit(wallet, txObject);
-                            if(gas.error !== undefined) {
-                                if(gas.error.message === "Returned error: execution reverted: CFA: flow does not exist") {
-                                    //register flow to recalculation
-                                    console.error("CFA: flow does not exist");
-                                    console.debug("Deleting from database");
-                                    await flow.destroy();
-                                    break;
-                                }
-                            } else {
-                                txObject.gasLimit = gas.gasLimit;
-                                console.log(txObject);
-                                console.debug("sending tx from account: ", wallet.address);
-                                networkAccountNonce++;
-                                const result = await this.sendWithRetry(wallet, txObject, this.timeout);
-                                if(result === undefined) {
-                                    console.error("error with tx");
-                                    //TODO: Resolve this type of errors.
-                                }
-                            }
-                        } catch(error) {
-                            console.error(error);
-                            process.exit(1);
-                        }
+                    this.app.logger.debug(`sending tx from account ${wallet.address}`);
+                    const result = await this.sendWithRetry(wallet, txObject, this.timeout);
+                    if(result !== undefined && result.error !== undefined) {
+                        this.app.logger.error(result.error);
                     } else {
-                        console.debug(`address ${flow.sender} is solvent at ${flow.superToken} with flow ${flow.flowRate}` );
-                        this.app.protocol.newEstimation(flow.superToken, flow.sender);
+                        this.app.logger.info(JSON.stringify(result));
                     }
+                } catch(err) {
+                    this.app.logger.error(err);
+                    process.exit(1);
                 }
+            } else {
+                this.app.logger.debug(`address ${job.sender} is solvent at ${job.superToken}`);
+                this.app.protocol.newEstimation(job.superToken, job.sender);
             }
         }
-        this.runningMux = false;
+    }
+
+    async multiTermination(batchWork, work) {
+        throw Error("Not implemented");
     }
 
     async sendWithRetry(wallet, txObject, ms) {
-
+        //gas limit estimation
+        const gas = await this.estimateGasLimit(wallet, txObject);
+        if(gas.error !== undefined) {
+            this.app.logger.error(gas.error);
+            if(gas.error.message === "Returned error: execution reverted: CFA - flow does not exist") {
+                await this.app.protocol.checkFlow(txObject.superToken, txObject.flowSender, txObject.flowReceiver);
+                return {error: gas.error, tx: undefined};
+            }
+        }
+        txObject.gasLimit = gas.gasLimit;
         const signed = await this.signTx(wallet, txObject);
         if(signed.error !== undefined) {
             console.log(signed.error);
             if(signed.error === "Returned error: replacement transaction underpriced") {
-                console.debug("replacement transaction underpriced")
+                this.app.logger.warn(`replacement transaction underpriced`);
                 txObject.retry = txObject.retry + 1;
                 return this.sendWithRetry(wallet, txObject, ms);
             }
 
             if(signed.error === "Returned error: execution reverted: CFA: flow does not exist") {
-                console.debug("Flow don't exist anymore - reclaim nonce");
-                return undefined;
+                this.app.logger.warn(`flow don't exist anymore`);
+                return {error: signed.error, tx: undefined};
             }
-
-            console.error(signed.error);
-            return undefined;
+            return {error: signed.error, tx: undefined};
         }
 
         try {
-            console.log("waiting until timeout");
+            this.app.logger.info(`waiting until timeout for ${this.timeout / 1000}`);
+            console.log(txObject);
             signed.tx.timeout = ms;
-            const tx =  await promiseTimeout(
+            const tx =  await utils.promiseTimeout(
                 this.app.client.sendSignedTransaction(signed),
                 ms
             );
 
-            return tx;
+            return {error: undefined, tx: tx};
 
-        } catch(error) {
-            if(error.message === "timeout rejection") {
-                console.debug(`agent account: ${wallet.address} - replacement with nonce : ${txObject.nonce} tx: ${signed.tx.transactionHash}`)
+        } catch(err) {
+            if(err.message === "timeout rejection") {
+                this.app.logger.debug(`agent account: ${wallet.address} - replacement with nonce: ${txObject.nonce} tx: ${signed.tx.transactionHash}`)
                 txObject.retry = txObject.retry + 1;
                 return this.sendWithRetry(wallet, txObject, ms);
             }
 
-            if(error.message === "Returned error: replacement transaction underpriced") {
-                txObject.retry = txObject.retry + 1;
-                txObject.gasPrice += 10;
+            if(err.message === "Returned error: replacement transaction underpriced") {
+                this.app.logger.debug(`replacing transaction underpriced`);
+                txObject.retry++;
                 return this.sendWithRetry(wallet, txObject, ms);
             }
+
+            if(err.message === "Error: Returned error: nonce too low") {
+                this.app.logger.debug(`nonce too low, retry`);
+                txObject.nonce++;
+                return this.sendWithRetry(wallet, txObject, ms);
+            }
+
+            if(err.message === "Error: Returned error: already known") {
+                this.app.logger.debug(`submited tx already known`);
+                return {error: err.message, tx: undefined};
+            }
+
+            console.log("catch error - ", err);
         }
     }
-
-
     async estimateGasLimit(wallet, txObject) {
         try {
             const result = await this.app.client.web3.eth.estimateGas({
@@ -182,9 +159,9 @@ class TxBuilder {
                 to: txObject.target,
                 data: txObject.tx
                 });
-            return {gasLimit : result, error: undefined};
-        } catch(error) {
-            return {gasLimit : undefined, error: error};
+            return { error: undefined, gasLimit : result };
+        } catch(err) {
+            return { error: err, gasLimit : undefined };
         }
     }
 
@@ -192,16 +169,15 @@ class TxBuilder {
         try {
             let gasPrice = txObject.gasPrice;
             if(txObject.retry > 1) {
-                console.log("update gas price");
                 console.log("old gasprice: ", txObject.gasPrice);
                 if(this.app.config.MAX_GAS_FEE !== undefined && parseInt(txObject.gasPrice) >= this.app.config.MAX_GAS_FEE) {
-                    console.log("Hit gas limit of ", this.app.config.MAX_GAS_FEE);
+                    this.app.logger.debug(`Hit gas price limit of ${this.app.config.MAX_GAS_FEE}`);
                     gasPrice = parseInt(txObject.gasPrice) + 1;
                 } else {
                     gasPrice = Math.ceil(parseInt(txObject.gasPrice) * txObject.step);
                 }
+                this.app.logger.debug(`update gas price from ${txObject.gasPrice} to ${gasPrice}`);
                 txObject.gasPrice = gasPrice;
-                console.log("new gasprice: ", gasPrice);
             }
 
             const unsignedTx = {
@@ -213,16 +189,18 @@ class TxBuilder {
                 gasPrice: gasPrice,
                 gasLimit : txObject.gasLimit
             };
+
+            this.app.logger.info("signing tx");
             const signed = await this.app.client.signTransaction(
                 unsignedTx,
                 wallet._privateKey.toString("hex")
             );
             signed.txObject = txObject;
             return { tx: signed, error: undefined };
-        } catch(error) {
-            return { tx: undefined, error: error};
+        } catch(err) {
+            return { tx: undefined, error: err};
         }
     }
 }
 
-module.exports = TxBuilder;
+module.exports = Liquidator;

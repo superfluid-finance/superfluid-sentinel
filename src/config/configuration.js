909d36cd686ad171cf031ee4cd8f6df17950a224
Axe <ngsmo@iscte-iul.pt>
2021-09-09 15:03:53 +0300

    match configurations

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -5,55 +5,51 @@ class Config {
 
     constructor(config) {
         if(typeof config === "object") {
-            this.WS_RPC_NODE = config.wsNode;
-            this.HTTP_RPC_NODE = config.httpNode;
+            this.HTTP_RPC_NODE = config.http_rpc_node;
+            this.WS_RPC_NODE = config.ws_rpc_node;
             this.MNEMONIC = config.mnemonic;
-            this.EPOCH_BLOCK = config.epochBlock;
-            this.DB = config.DB;
-            process.env.DB = this.DB;
-            this.PROTOCOL_RELEASE_VERSION = config.prv;
-            this.TX_TIMEOUT = config.timeoutFn;
-            this.MAX_QUERY_BLOCK_RANGE = config.blockRange;
-            this.CONCURRENCY = config.concurrency;
-            this.COLD_BOOT = config.coldBoot;
-            this.LISTEN_MODE = config.listenMode;
-            this.NUM_RETRIES = config.numberRetries;
-            this.TEST_RESOLVER = config.testResolver
-            this.shutdownOnError = false;
-            this.ADDITIONAL_LIQUIDATION_DELAY = config.liquidationDelay || 0;
-            this.MAX_GAS_FEE = config.maxFee;
-            this.CLO_ADDR = config.cloAddr;
-            this.PRIVATE_KEY = config.privateKey;
-            this.CHAIN_ID = config.chainId;
-        } else {
-            this.WS_RPC_NODE = process.env.WS_RPC_NODE;
-            this.HTTP_RPC_NODE = process.env.HTTP_RPC_NODE;
-            this.MNEMONIC = process.env.MNEMONIC;
-            if(process.env.PATH_DB !== undefined && process.env.PATH_DB !== "") {
-                this.DB = process.env.PATH_DB;
-            } else {
-                this.DB = "./datadir/db.sqlite";
+            this.PRIVATE_KEY = config.private_key;
+            this.MAX_QUERY_BLOCK_RANGE = config.max_query_block_range || 2000;
+            if(config.tokens !== undefined && config.tokens !== "") {
+                this.TOKENS = config.tokens.split(",");
             }
-            this.PROTOCOL_RELEASE_VERSION = process.env.PROTOCOL_RELEASE_VERSION;
-            this.TX_TIMEOUT = process.env.TX_TIMEOUT || 60000;
-            this.MAX_QUERY_BLOCK_RANGE = process.env.MAX_QUERY_BLOCK_RANGE || 2000;
-            this.COLD_BOOT = process.env.COLD_BOOT == 1 ? true : false;
-            this.ADDITIONAL_LIQUIDATION_DELAY = process.env.ADDITIONAL_LIQUIDATION_DELAY || 0;
-            this.MAX_GAS_FEE = process.env.MAX_GAS_FEE;
-            this.CLO_ADDR = process.env.CLO_ADDR;
-            this.PRIVATE_KEY = process.env.PRIVATE_KEY;
-            this.RETRY_GAS_MULTIPLIER = process.env.RETRY_GAS_MULTIPLIER || 1.15;
+            this.DB = (config.path_db !== undefined && config.path_db !== "") ? config.path_db : "./datadir/db.sqlite";
+            this.ADDITIONAL_LIQUIDATION_DELAY = config.additional_liquidation_delay || 0;
+            this.TX_TIMEOUT = config.tx_timeout || 60000;
+            this.PROTOCOL_RELEASE_VERSION = config.protocol_release_version;
+            this.MAX_GAS_PRICE = config.max_gas_price;
+            this.RETRY_GAS_MULTIPLIER = config.retry_gas_multiplier || 1.15;
+            this.CLO_ADDR = config.clo_addr;
+            
+            this.CONCURRENCY = config.concurrency;
+            this.COLD_BOOT = config.cold_boot;
+            this.LISTEN_MODE = config.listen_mode;
+            this.NUM_RETRIES = config.number_retries;
+            this.TEST_RESOLVER = config.test_resolver;
+            this.shutdownOnError = config.shutdown_on_error;
 
+        } else {
+            this.HTTP_RPC_NODE = process.env.HTTP_RPC_NODE;
+            this.WS_RPC_NODE = process.env.WS_RPC_NODE;
+            this.MNEMONIC = process.env.MNEMONIC;
+            this.PRIVATE_KEY = process.env.PRIVATE_KEY;
+            this.MAX_QUERY_BLOCK_RANGE = process.env.MAX_QUERY_BLOCK_RANGE || 2000;
             if(process.env.TOKENS !== undefined && process.env.TOKENS !== "") {
                 this.TOKENS = process.env.TOKENS.split(",");
             }
+            this.DB = (process.env.PATH_DB !== undefined && process.env.PATH_DB !== "") ? process.env.PATH_DB : "./datadir/db.sqlite";
+            this.ADDITIONAL_LIQUIDATION_DELAY = process.env.ADDITIONAL_LIQUIDATION_DELAY || 0;
+            this.TX_TIMEOUT = process.env.TX_TIMEOUT || 60000;
+            this.PROTOCOL_RELEASE_VERSION = process.env.PROTOCOL_RELEASE_VERSION;
+            this.MAX_GAS_PRICE = process.env.MAX_GAS_PRICE;
+            this.RETRY_GAS_MULTIPLIER = process.env.RETRY_GAS_MULTIPLIER || 1.15;
+            this.CLO_ADDR = process.env.CLO_ADDR;
 
             this.CONCURRENCY = 1;
             this.LISTEN_MODE = 1;
             this.NUM_RETRIES = 10;
             this.shutdownOnError = false;
         }
-
     }
 
     loadNetworkInfo(chainId) {
--- src/config/loadCmdArgs.js
+++ src/config/loadCmdArgs.js
@@ -6,15 +6,17 @@ program
     .description("Superfluid Community Agent")
     .option("-h, --http-eth-node [value]", "HTTP ETH Node URL")
     .option("-w, --ws-eth-node [value]", "WebSocket ETH Node URL")
-    .option("-m, --mnemonic [value]","Node Wallet Mnemonic")
     .option("-p, --private-key [value]","Node Private Key")
-    .option("-n, --token [value]", "Addresses of SuperToken agent should perform liquidation")
+    .option("-m, --mnemonic [value]","Node Wallet Mnemonic")
+    .option("-mb, --max-query-block-range [value]", "max query block range")
+    .option("-t, --tokens [value]", "Addresses of SuperToken agent should perform liquidation")
     .option("-d, --path-db [value]", "Directory and DB to save/load agent information")
-    .option("-c, --chain-id [value]", "Network to run")
-    .option("-l, --liquidation-delay [value]", "delay liquidaton in seconds")
-    .option("-c, --chain-id [value]", "Network to run")
-    .option("-c, --chain-id [value]", "Network to run")
-    .option("-c, --chain-id [value]", "Network to run")
+    .option("-d, --additional-liquidation-delay [value]", "addition liquidaton delay in seconds")
+    .option("-to, --tx-timeout [value]", "Network to run")
+    .option("-p, --protocol-release-version [value]", "Protocol Release Version")
+    .option("-g, --max-gas-price [value]", "Max gas price for a liquidation")
+    .option("-r, --retry-gas-multiplier [value]", "Retry Gas Multiplier")
+    .option("-a, --clo-address [value]", "CLO Address")
     .action(function (args) {
         if(args.httpEthNode !== undefined) {
             process.env.HTTP_RPC_NODE = args.httpEthNode;
@@ -28,14 +30,32 @@ program
         if(args.privateKey !== undefined) {
             process.env.PRIVATE_KEY = args.privateKey;
         }
+        if(args.maxQueryBlockRange !== undefined) {
+            process.env.MAX_QUERY_BLOCK_RANGE = args.maxQueryBlockRange;
+        }
         if(args.tokens !== undefined) {
             process.env.TOKENS = args.tokens;
         }
         if(args.pathDb !== undefined) {
             process.env.PATH_DB = args.pathDb;
         }
-        if(args.chainId !== undefined) {
-            process.env.CHAIN_ID = args.chainId;
+        if(args.additionalLiquidationDelay !== undefined) {
+            process.env.ADDITIONAL_LIQUIDATION_DELAY = args.additionalLiquidationDelay;
+        }
+        if(args.txTimeout !== undefined) {
+            process.env.TX_TIMEOUT = args.txTimeout;
+        }
+        if(args.protocolReleaseVersion !== undefined) {
+            process.env.PROTOCOL_RELEASE_VERSION = args.protocolReleaseVersion;
+        }
+        if(args.maxGasPrice !== undefined) {
+            process.env.MAX_GAS_PRICE = args.maxGasPrice;
+        }
+        if(args.retryGasMultiplier !== undefined) {
+            process.env.RETRY_GAS_MULTIPLIER = args.retryGasMultiplier;
+        }
+        if(args.cloAddress !== undefined) {
+            process.env.CLO_ADDR = args.cloAddress;
         }
     });
 program.parse(process.argv);

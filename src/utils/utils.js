d286ace89b0b79b2a7e5bec5424da6fd5d575e20
Axe <ngsmo@iscte-iul.pt>
2022-01-26 11:16:31 +0000

    add functions to download and unzip file

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,9 @@
 const bip39 = require("bip39");
 const { hdkey } = require("ethereumjs-wallet");
+const fs = require("fs");
+const path = require('path')
+const axios = require("axios")
+const zlib = require("zlib");
 
 function generateAccounts (mnemonic, accountIndex) {
   const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeedSync(mnemonic));
@@ -17,7 +21,37 @@ function sortString (a, b) {
   return x === y ? 0 : x > y ? 1 : -1;
 }
 
+function fileExist(path) {
+  return fs.existsSync(path);
+}
+
+async function downloadFile(url, dest, retries=3) {
+  const writer = fs.createWriteStream(dest);
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  });
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve)
+    writer.on('error', reject)
+  })
+
+}
+
+function unzip(file) {
+  const rd = fs.readFileSync(file);
+  const data = zlib.gunzipSync(rd);
+  fs.writeFileSync(`${file.slice(0, -3)}`, data);
+  fs.unlinkSync(file);
+}
+
 module.exports = {
   generateAccounts,
-  sortString
+  sortString,
+  fileExist,
+  downloadFile,
+  unzip
 };

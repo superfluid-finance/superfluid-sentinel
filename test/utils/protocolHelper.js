d472e64694517827e00c430d5460f06e183acf46
Axe <ngsmo@iscte-iul.pt>
2022-02-03 15:16:43 +0000

    liquidations expects

--- test/utils/protocolHelper.js
+++ test/utils/protocolHelper.js
@@ -8,6 +8,7 @@ const IToken = require("@superfluid-finance/ethereum-contracts/build/contracts/T
 const TOGA = require("@superfluid-finance/ethereum-contracts/build/contracts/TOGA.json");
 
 const Web3 = require("web3");
+const expect = require("chai").expect;
 
 async function setup(provider, agentAccount) {
 
@@ -74,6 +75,99 @@ async function setup(provider, agentAccount) {
     };
 }
 
+function expectLiquidation(event, node, account) {
+    expect(event.returnValues.liquidatorAccount).to.equal(node);
+    expect(event.returnValues.bailoutAmount).to.equal("0");
+    expect(event.returnValues.penaltyAccount).to.equal(account);
+};
+
+function expectBailout(event, node, account) {
+    expect(event.returnValues.liquidatorAccount).to.equal(node);
+    expect(event.returnValues.bailoutAmount).not.equal("0");
+    expect(event.returnValues.penaltyAccount).to.equal(account);
+};
+
+function getSentinelConfig(config) {
+    const myBaseConfig = {
+        http_rpc_node: "http://127.0.0.1:8545",
+        mnemonic: "clutch mutual favorite scrap flag rifle tone brown forget verify galaxy return",
+        mnemonic_index: 100,
+        epoch_block: 0,
+        db_path: "datadir/testing/test.sqlite",
+        protocol_release_version: "test",
+        log_level: "info",
+        tx_timeout: 20,
+        max_query_block_range: 500000,
+        max_gas_price: 4000000000,
+        concurrency: 1,
+        cold_boot: 1,
+        only_listed_tokens: 1,
+        number_retries: 3,
+        additional_liquidation_delay: 0,
+        block_offset: 1,
+        liquidation_job_awaits: 5000,
+        fastsync: "false"
+    };
+
+    return { ...myBaseConfig, ...config };
+}
+
+function exitWithError(error) {
+    console.error(error);
+    process.exit(1);
+};
+
+async function waitForEvent(protocolVars, sentinel, ganache, eventName, blockNumber) {
+    while (true) {
+        try {
+            const newBlockNumber = await protocolVars.web3.eth.getBlockNumber();
+            console.log(`${blockNumber} - ${newBlockNumber}`);
+            const events = await protocolVars.superToken.getPastEvents(eventName, {
+                fromBlock: blockNumber,
+                toBlock: newBlockNumber
+            });
+            if (events.length > 0) {
+                return events;
+            }
+            await timeout(1000);
+            await ganache.helper.timeTravelOnce(1, sentinel, true);
+        } catch (err) {
+            exitWithError(err);
+        }
+    }
+};
+
+//TODO:REFACTOR
+async function waitForEventAtSameBlock(protocolVars, sentinel, ganache, eventName, numberOfEvents, blockNumber) {
+    while (true) {
+        try {
+            console.log(`checking block: ${blockNumber}`);
+            const events = await protocolVars.superToken.getPastEvents(eventName, {
+                fromBlock: blockNumber,
+                toBlock: blockNumber
+            });
+            if (events.length === numberOfEvents) {
+                return Number(events[0].blockNumber);
+            }
+            await timeout(1000);
+            await ganache.helper.timeTravelOnce(1, sentinel, true);
+            blockNumber += 1;
+        } catch (err) {
+            exitWithError(err);
+        }
+    }
+};
+async function timeout(ms) {
+    return new Promise(res => setTimeout(res, ms));
+}
+
 module.exports = {
-    setup
+    setup,
+    expectLiquidation,
+    expectBailout,
+    getSentinelConfig,
+    timeout,
+    waitForEvent,
+    waitForEventAtSameBlock,
+    exitWithError
 };

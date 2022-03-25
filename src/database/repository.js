4224dd900480a47a01e1de3609177c23434cde03
ngmachado <ngsmo@iscte-iul.pt>
2022-03-25 22:16:17 +0000

    fix tokens filtering on batching checking

--- src/database/repository.js
+++ src/database/repository.js
@@ -129,6 +129,15 @@ having count(*) > 1
 order by count(*) desc`;
 
 
+        if (inSnipped !== "") {
+            return this.app.db.query(sqlquery, {
+                replacements: {
+                    dt: checkDate,
+                    tokens: onlyTokens
+                },
+                type: QueryTypes.SELECT
+            });
+        }
         return this.app.db.query(sqlquery, {
             replacements: {dt: checkDate},
             type: QueryTypes.SELECT

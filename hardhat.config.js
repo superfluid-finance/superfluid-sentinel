6c9e0250961364b7b220802a6fb008160b40bd97
Axe <seamac@gmail.com>
2023-07-15 16:57:20 +0100

    clean hardhat install

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,6 @@
-require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: "0.8.19",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      }
-    },
-    networks: {
-      localhost: {
-        url: "http://127.0.0.1:8545/",
-        chainId: 31337,
-      }
-    }
-  }
+  solidity: "0.8.19",
 };

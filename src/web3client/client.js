c46474bf95f5edbe3f238b602394268b4e8328fb
Axe <seamac@gmail.com>
2023-07-23 10:51:54 +0100

    refactor to use new classes and drop local impl.

--- src/web3client/client.js
+++ src/web3client/client.js
@@ -1,21 +1,14 @@
 const { Web3 } = require('web3');
-const IResolver = require("@superfluid-finance/ethereum-contracts/build/contracts/IResolver.json");
-const ICFA = require("@superfluid-finance/ethereum-contracts/build/contracts/IConstantFlowAgreementV1.json");
-const IGDA = require("@superfluid-finance/ethereum-contracts/build/contracts/IGeneralDistributionAgreementV1.json");
-const IIDA = require("@superfluid-finance/ethereum-contracts/build/contracts/IInstantDistributionAgreementV1.json");
-const ISuperfluid = require("@superfluid-finance/ethereum-contracts/build/contracts/ISuperfluid.json");
-const ISuperToken = require("@superfluid-finance/ethereum-contracts/build/contracts/ISuperToken.json");
-const SuperfluidGovernance = require("@superfluid-finance/ethereum-contracts/build/contracts/SuperfluidGovernanceBase.json");
-const BatchContract = require("@superfluid-finance/ethereum-contracts/build/contracts/BatchLiquidator.json");
-const TogaContract = require("@superfluid-finance/ethereum-contracts/build/contracts/TOGA.json");
 const { wad4human } = require("@decentral.ee/web3-helpers");
 const BN = require("bn.js");
+const ISuperToken = require("@superfluid-finance/ethereum-contracts/build/contracts/ISuperToken.json");
 
 const AccountManager = require("./accountManager");
 const RPCClient = require("./rpcClient");
+const Contracts = require("./contracts");
+const SuperTokenManager = require("./superTokenManager");
 
 const { FMT_NUMBER, FMT_BYTES } = require("web3");
-
 const dataFormat = {
   number: FMT_NUMBER.NUMBER
 }
@@ -43,6 +36,9 @@ class Client {
       await this.RPCClient.connect();
       this.accountManager = new AccountManager(this.RPCClient.web3);
 
+      // we need web3 instance for contract loader
+      this.contracts = new Contracts(this.app);
+
     } catch(err) {
       this.app.logger.error(err);
       throw new Error(`Client.RPC.connect(): ${err}`);
@@ -51,12 +47,20 @@ class Client {
 
   async init () {
     try {
+      this.app.logger.info(`Web3Client start`);
       if(!this.RPCClient.isConnected) {
         throw Error(`Client.init(): not connected to rpc`);
       }
-      this.app.logger.info(`Web3Client start`);
-      this.app.logger.info(`ChainId: ${await this.RPCClient.getChainId()}`)
-      await this._loadSuperfluidContracts();
+
+      this.app.logger.info(`ChainId: ${await this.RPCClient.getChainId()}`);
+
+      // Load contracts
+      await this.contracts.initialize();
+
+      this.superToken = new SuperTokenManager(this.app);
+
+
+      // Account Manager - Load account
       if (this.app.config.PRIVATE_KEY !== undefined) {
         this.accountManager.addAccountFromPrivateKey(this.app.config.PRIVATE_KEY);
       } else if (this.app.config.MNEMONIC !== undefined) {
@@ -83,160 +87,6 @@ class Client {
     }
   }
 
-  async loadBatchContract () {
-    try {
-      if (this.app.config.BATCH_CONTRACT !== undefined) {
-        this.batch = new this.RPCClient.web3.eth.Contract(BatchContract.abi, this.app.config.BATCH_CONTRACT);
-      } else {
-        this.app.logger.info("Batch Contract not found");
-      }
-    } catch (err) {
-      this.app.logger.error(err);
-      throw Error(`Client.loadBatchContract() : ${err}`);
-    }
-  }
-
-  async loadTogaContract () {
-    try {
-      if (this.app.config.TOGA_CONTRACT !== undefined) {
-        this.toga = new this.RPCClient.web3.eth.Contract(TogaContract.abi, this.app.config.TOGA_CONTRACT);
-      } else {
-        this.app.logger.info("TOGA Contract not found");
-      }
-    } catch (err) {
-      this.app.logger.error(err);
-      throw Error(`Client.loadTogaContract() : ${err}`);
-    }
-  }
-
-  async _loadSuperfluidContracts () {
-    try {
-
-      this.resolver = new this.RPCClient.web3.eth.Contract(IResolver.abi,this.app.config.RESOLVER);
-      const superfluidAddress = await this.resolver.methods.get(`Superfluid.${this.version}`).call();
-      this.sf = new this.RPCClient.web3.eth.Contract(ISuperfluid.abi,superfluidAddress);
-      const govAddress = await this.sf.methods.getGovernance().call();
-      this.gov = new this.RPCClient.web3.eth.Contract(SuperfluidGovernance.abi, govAddress);
-      // Agreements
-      const cfaIdent = this.RPCClient.web3.utils.sha3("org.superfluid-finance.agreements.ConstantFlowAgreement.v1");
-      const gdaIdent = this.RPCClient.web3.utils.sha3("org.superfluid-finance.agreements.GeneralDistributionAgreement.v1");
-      const idaIdent = this.RPCClient.web3.utils.sha3("org.superfluid-finance.agreements.InstantDistributionAgreement.v1");
-      const [cfaAddress, idaAddress, gdaAddress] = await Promise.all([
-          this.sf.methods.getAgreementClass(cfaIdent).call(),
-          this.sf.methods.getAgreementClass(idaIdent).call(),
-          this.sf.methods.getAgreementClass(gdaIdent).call()
-      ]);
-
-      this.CFAv1 = new this.RPCClient.web3.eth.Contract(ICFA.abi, cfaAddress);
-      this.IDAv1 = new this.RPCClient.web3.eth.Contract(IIDA.abi, idaAddress);
-      this.GDAv1 = new this.RPCClient.web3.eth.Contract(IGDA.abi, gdaAddress);
-
-      this.app.logger.info(`Resolver: ${this.app.config.RESOLVER}`);
-      this.app.logger.info(`Superfluid: ${superfluidAddress}`);
-      this.app.logger.info(`Superfluid Governance: ${govAddress}`);
-      this.app.logger.info(`CFA address: ${cfaAddress}`);
-      this.app.logger.info(`GDA address: ${gdaAddress}`);
-      this.app.logger.info(`IDA address: ${idaAddress}`);
-
-    } catch (err) {
-      this.app.logger.error(err);
-      throw Error(`Client._loadSuperfluidContracts(): ${err}`);
-    }
-  }
-
-  async _loadSuperTokensFromDB () {
-    try {
-      let filter = {
-        attributes: ["address"]
-      };
-
-      if (this.app.config.ONLY_LISTED_TOKENS === true) {
-        filter = {
-          attributes: ["address"],
-          where: { listed: 1 }
-        };
-      }
-      const superTokensDB = await this.app.db.models.SuperTokenModel.findAll(filter);
-      for(const token of superTokensDB) {
-        await this.loadSuperToken(token.address);
-      }
-    } catch (err) {
-      this.app.logger.error(err);
-      throw new Error(`Client._loadSuperTokensFromDB(): ${err}`);
-    }
-  }
-
-  async loadSuperTokens (newSuperTokens) {
-    try {
-      await this._loadSuperTokensFromDB();
-      const promises = newSuperTokens.map(async (token) => {
-        return this.loadSuperToken(token);
-      });
-      await Promise.all(promises);
-    } catch (err) {
-      this.app.logger.error(err);
-      throw new Error(`Client.loadSuperTokens(): ${err}`);
-    }
-  }
-
-  async loadSuperToken (newSuperToken, setPIC=false) {
-    if (this.superTokens[newSuperToken.toLowerCase()] !== undefined) {
-      return;
-    }
-    const superTokenHTTP = new this.RPCClient.web3.eth.Contract(ISuperToken.abi, newSuperToken);
-    const [tokenName, tokenSymbol] = await Promise.all(
-      [
-        superTokenHTTP.methods.name().call(),
-        superTokenHTTP.methods.symbol().call()
-      ]
-    );
-
-    //get liquidation period
-    const resp = await this.gov.methods.getPPPConfig(this.sf._address, newSuperToken).call();
-    // if liquidation period and patrician period are not set
-    if (resp.liquidationPeriod === "0" && resp.patricianPeriod === "0") {
-      this.app.logger.error(`Liquidation period and patrician period are 0 for ${tokenSymbol} - ${tokenName} (${newSuperToken})`);
-    }
-    superTokenHTTP.liquidation_period = parseInt(resp.liquidationPeriod);
-    superTokenHTTP.patrician_period = parseInt(resp.patricianPeriod);
-    const superTokenAddress = await this.resolver.methods.get(
-      `supertokens.${this.version}.${tokenSymbol}`
-    ).call();
-
-    let isListed = superTokenAddress === newSuperToken;
-    if (this.app.config.ONLY_LISTED_TOKENS === true && isListed) {
-      const tokenInfo = `SuperToken (${tokenSymbol} - ${tokenName}): ${superTokenAddress}`;
-      this.app.logger.info(tokenInfo);
-      this.superTokenNames[newSuperToken.toLowerCase()] = tokenInfo;
-      this.superTokens[superTokenAddress.toLowerCase()] = superTokenHTTP;
-      this.superTokensAddresses.push(superTokenAddress.toLowerCase());
-      isListed = 1;
-    } else {
-      const tokenInfo = `(${tokenSymbol} - ${tokenName}): ${newSuperToken}`;
-      this.app.logger.info(tokenInfo);
-      this.superTokenNames[newSuperToken.toLowerCase()] = tokenInfo;
-      this.superTokens[newSuperToken.toLowerCase()] = superTokenHTTP;
-      this.superTokensAddresses.push(newSuperToken.toLowerCase());
-    }
-    // persistence database
-    await this.app.db.models.SuperTokenModel.upsert({
-      address: newSuperToken,
-      symbol: tokenSymbol,
-      name: tokenName,
-      liquidationPeriod: parseInt(resp.liquidationPeriod),
-      patricianPeriod: parseInt(resp.patricianPeriod),
-      listed: isListed
-    });
-    // use for runtime subscription
-    if(setPIC) {
-      this.app.protocol.calculateAndSaveTokenDelay(newSuperToken, false);
-    }
-  }
-
-  isSuperTokenRegistered (token) {
-    return this.superTokens[token.toLowerCase()] !== undefined;
-  }
-
   async getChainId () {
     if (this.chainId === undefined) {
       this.chainId = Number(await this.RPCClient.web3.eth.getChainId());
@@ -252,7 +102,7 @@ class Client {
     return this.accountManager.getAccountBalance(0);
   }
 
-    async isAccountBalanceBelowMinimum () {
+  async isAccountBalanceBelowMinimum () {
       return this.accountManager.isAccountBalanceBelowMinimum(0, this.app.config.SENTINEL_BALANCE_THRESHOLD)
     }
 
@@ -298,8 +148,9 @@ class Client {
   }
 
   getSFAddresses () {
-    const togaAddress = this.toga !== undefined ? this.toga._address : undefined;
-    return [...this.superTokensAddresses, this.IDAv1._address, this.CFAv1._address, this.GDAv1._address, togaAddress].filter(n => n);
+    const agreementsAddresses = this.contracts.getAgreementsAddresses();
+    const togaAddress = this.contracts.getTogaAddress();
+    return [...this.superToken.superTokensAddresses, ...agreementsAddresses, togaAddress].filter(n => n);
   }
 
   getTotalRequests () {

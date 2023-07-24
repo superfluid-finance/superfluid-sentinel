200bcd6b50180bb5edd4e681cc927527738bd00b
Axe <seamac@gmail.com>
2023-07-24 16:07:45 +0100

    fix CFA for new Toga deployed

--- test/integration/cfa.integration.test.js
+++ test/integration/cfa.integration.test.js
@@ -58,7 +58,7 @@ describe("CFA tests", () => {
     await ganache.close();
   });
 
-  it("Create one stream", async () => {
+  it("#1.1 - create one stream", async () => {
     try {
       await helper.operations.createStream(helper.sf.superToken.options.address, accounts[0], accounts[2], "100000000000");
       await ganache.helper.timeTravelOnce(provider, web3,1);
@@ -77,7 +77,7 @@ describe("CFA tests", () => {
     }
   });
 
-  it("Create small stream then updated to bigger stream", async () => {
+  it("#1.2 - create small stream then updated to bigger stream", async () => {
     try {
       await helper.operations.createStream(helper.sf.superToken.options.address, accounts[0], accounts[2], "1000");
       await ganache.helper.timeTravelOnce(provider, web3,1);
@@ -97,7 +97,7 @@ describe("CFA tests", () => {
     }
   });
 
-  it("Create one out going stream and receive a smaller incoming stream", async () => {
+  it("#1.3 - create one out going stream and receive a smaller incoming stream", async () => {
     try {
       await helper.operations.createStream(helper.sf.superToken.options.address, accounts[0], accounts[2], "1000000000");
       await ganache.helper.timeTravelOnce(provider, web3,1);
@@ -117,7 +117,7 @@ describe("CFA tests", () => {
     }
   });
 
-  it("Create two outgoing streams, and new total outflow rate should apply to the agent estimation logic", async () => {
+  it("#1.4 - create two outgoing streams, and new total outflow rate should apply to the agent estimation logic", async () => {
     try {
       await helper.operations.createStream(helper.sf.superToken.options.address, accounts[0], accounts[2], "1000000000000000");
       await ganache.helper.timeTravelOnce(provider, web3,1);
@@ -136,7 +136,7 @@ describe("CFA tests", () => {
     }
   });
 
-  it("Create a stream with big flow rate, then update the stream with smaller flow rate", async () => {
+  it("#1.5 - create a stream with big flow rate, then update the stream with smaller flow rate", async () => {
     try {
       await helper.operations.createStream(helper.sf.superToken.options.address, accounts[5], accounts[2], "100000000000000");
       await ganache.helper.timeTravelOnce(provider, web3, 1);
@@ -156,28 +156,20 @@ describe("CFA tests", () => {
       protocolHelper.exitWithError(err);
     }
   });
-  //TODO: need toga to fix this test
-  it.skip("Should make liquidation as Pleb", async() => {
+
+  it("#1.6 - should make liquidation as Pleb", async() => {
     try {
-      const data = protocolVars.cfa.methods.createFlow(
-          protocolVars.superToken._address,
-          accounts[2],
-          "10000000000000000",
-          "0x"
-      ).encodeABI();
-      await protocolVars.host.methods.callAgreement(protocolVars.cfa._address, data, "0x").send({
+      await helper.operations.createStream(helper.sf.superToken.options.address, accounts[0], accounts[2], "100000000000000");
+      await ganache.helper.timeTravelOnce(provider, web3, 1);
+      await bootNode({resolver: helper.sf.resolver.options.address, log_level: "debug", toga_contract: helper.togaAddress});
+      await ganache.helper.timeTravelOnce(provider, web3, 60);
+      const tx = await helper.sf.superToken.methods.transferAll(accounts[9]).send({
         from: accounts[0],
         gas: 1000000
       });
-      await ganache.helper.timeTravelOnce(1);
-      await bootNode({log_level:"debug"});
-      await ganache.helper.timeTravelOnce(60);
-      const tx = await protocolVars.superToken.methods.transferAll(accounts[2]).send({
-        from: accounts[0],
-        gas: 1000000
-      });
-      await ganache.helper.timeTravelOnce(900);
-      const result = await protocolHelper.waitForEvent(protocolVars, app, ganache, "AgreementLiquidatedV2", tx.blockNumber);
+      await ganache.helper.timeTravelOnce(provider, web3, 3580, app, true);
+
+      const result = await protocolHelper.waitForEvent(helper, app, ganache, "AgreementLiquidatedV2", tx.blockNumber);
       await app.shutdown();
       protocolHelper.expectLiquidationV2(result[0], AGENT_ACCOUNT, accounts[0], "1");
     } catch (err) {
@@ -185,57 +177,18 @@ describe("CFA tests", () => {
     }
   });
 
-  //TODO: need toga to fix this test
-  it.skip("Should make liquidation wait until Pleb slot", async() => {
+  it("#1.7 - should make liquidation as Pirate", async() => {
     try {
-      const data = protocolVars.cfa.methods.createFlow(
-          protocolVars.superToken._address,
-          accounts[2],
-          "10000000000000000",
-          "0x"
-      ).encodeABI();
-      await protocolVars.host.methods.callAgreement(protocolVars.cfa._address, data, "0x").send({
+      await helper.operations.createStream(helper.sf.superToken.options.address, accounts[0], accounts[2], "100000000000000");
+      await ganache.helper.timeTravelOnce(provider, web3, 1);
+      await bootNode({resolver: helper.sf.resolver.options.address, log_level: "debug", toga_contract: helper.togaAddress, pirate: true});
+      await ganache.helper.timeTravelOnce(provider, web3, 60);
+      const tx = await helper.sf.superToken.methods.transferAll(accounts[9]).send({
         from: accounts[0],
         gas: 1000000
       });
-      await ganache.helper.timeTravelOnce(1);
-      await bootNode({log_level:"debug"});
-      await ganache.helper.timeTravelOnce(60);
-      const tx = await protocolVars.superToken.methods.transferAll(accounts[2]).send({
-        from: accounts[0],
-        gas: 1000000
-      });
-      await ganache.helper.timeTravelOnce(850);
-      const result = await protocolHelper.waitForEvent(protocolVars, app, ganache, "AgreementLiquidatedV2", tx.blockNumber);
-      await app.shutdown();
-      protocolHelper.expectLiquidationV2(result[0], AGENT_ACCOUNT, accounts[0], "1");
-    } catch (err) {
-      protocolHelper.exitWithError(err);
-    }
-  });
-
-  //TODO: need toga to fix this test
-  it.skip("Should make liquidation as Pirate", async() => {
-    try {
-      const data = protocolVars.cfa.methods.createFlow(
-          protocolVars.superToken._address,
-          accounts[2],
-          "10000000000000000",
-          "0x"
-      ).encodeABI();
-      await protocolVars.host.methods.callAgreement(protocolVars.cfa._address, data, "0x").send({
-        from: accounts[0],
-        gas: 1000000
-      });
-      await ganache.helper.timeTravelOnce(1);
-      await bootNode({pirate: "true"});
-      await ganache.helper.timeTravelOnce(60);
-      const tx = await protocolVars.superToken.methods.transferAll(accounts[2]).send({
-        from: accounts[0],
-        gas: 1000000
-      });
-      await ganache.helper.timeTravelOnce(14400);
-      const result = await protocolHelper.waitForEvent(protocolVars, app, ganache, "AgreementLiquidatedV2", tx.blockNumber);
+      await ganache.helper.timeTravelOnce(provider, web3, 14400, app, true);
+      const result = await protocolHelper.waitForEvent(helper, app, ganache, "AgreementLiquidatedV2", tx.blockNumber);
       await app.shutdown();
       protocolHelper.expectLiquidationV2(result[0], AGENT_ACCOUNT, accounts[0], "2");
     } catch (err) {
@@ -243,7 +196,7 @@ describe("CFA tests", () => {
     }
   });
 
-  it("Subscribe to token runtime", async () => {
+  it("#1.8 - subscribe to token runtime", async () => {
     try {
       await bootNode({pic: ZERO_ADDRESS, resolver: helper.sf.resolver.options.address, log_level: "debug", toga_contract: helper.togaAddress});
       await helper.operations.createStream(helper.sf.superToken.options.address, accounts[0], accounts[2], "1000000000");
